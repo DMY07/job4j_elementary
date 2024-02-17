57e7c8c1af5f455055dceb52502f2d842235884f
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-02-17 22:54:59 +0500

    4.2. Заголовок в файле java [#159036 #481820]

--- src/main/java/ru/job4j/info/Lis.java
+++ src/main/java/ru/job4j/info/Lis.java
@@ -2,6 +2,6 @@ package ru.job4j.info;
 
 public class Lis {
     public static void main(String[] args) {
-        System.out.println("17.02.21124");
+        System.out.println("17.02.2024");
     }
 }
