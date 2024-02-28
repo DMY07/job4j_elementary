65952c083c61704c164cb7bb59f945f49653e4f8
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-02-28 22:12:40 +0500

    1.2. Входные данные в тесте [#297750 #483480]

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -5,12 +5,42 @@ public class X2 {
         return a * x * x + b * x + c;
     }
 
+    public static int calc1(int a1, int b1, int c1, int x1) {
+        return a1 * x1 * x1 + b1 * x1 + c1;
+    }
+
+    public static int calc2(int a2, int b2, int c2, int x2) {
+        return a2 * x2 * x2 + b2 * x2 + c2;
+    }
+
+    public static int calc3(int a3, int b3, int c3, int x3) {
+        return a3 * x3 * x3 + b3 * x3 + c3;
+    }
+
     public static void main(String[] args) {
-        int a = 10;
-        int b = 0;
-        int c = 0;
-        int x = 2;
-        int result = X2.calc(a, b, c, x);
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int a1 = 0;
+        int b1 = 1;
+        int c1 = 1;
+        int x1 = 1;
+        int a2 = 1;
+        int b2 = 1;
+        int c2 = 0;
+        int x2 = 1;
+        int a3 = 1;
+        int b3 = 1;
+        int c3 = 1;
+        int x3 = 0;
+        int result = X3.calc(a, b, c, x);
+        int result1 = X3.calc1(a1, b1, c1, x1);
+        int result2 = X3.calc2(a2, b2, c2, x2);
+        int result3 = X3.calc3(a3, b3, c3, x3);
         System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
     }
 }
--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -1,19 +1,53 @@
 package ru.job4j.condition;
 
-import static org.assertj.core.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 class X2Test {
 
     @Test
-    void whenA10B0C0X2Then40() {
-        int a = 10;
-        int b = 0;
-        int c = 0;
-        int x = 2;
-        int expected = 40;
+    void whenA1B1C1X1Then3() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 3;
         int result = X2.calc(a, b, c, x);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenA0B1C1X1Then2() {
+        int a1 = 0;
+        int b1 = 1;
+        int c1 = 1;
+        int x1 = 1;
+        int expected = 2;
+        int result = X2.calc(a1, b1, c1, x1);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenA1B1C0X1Then2() {
+        int a2 = 1;
+        int b2 = 1;
+        int c2 = 0;
+        int x2 = 1;
+        int expected = 2;
+        int result = X2.calc(a2, b2, c2, x2);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenA1B1C1X0Then2() {
+        int a3 = 1;
+        int b3 = 1;
+        int c3 = 1;
+        int x3 = 0;
+        int expected = 1;
+        int result = X2.calc(a3, b3, c3, x3);
         assertThat(result).isEqualTo(expected);
     }
 }
