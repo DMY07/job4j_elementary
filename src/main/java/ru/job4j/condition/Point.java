48efae20630b2b43872ea169849d6ed6d8e9d3aa
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-02-28 23:29:31 +0500

    3. Тесты для определения расстояния между точками. [#159459]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -6,8 +6,22 @@ public class Point {
         return result;
     }
 
+    public static double distance1(int x3, int y3, int x4, int y4) {
+        double result1 = Math.sqrt(Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
+        return result1;
+    }
+
+    public static double distance2(int x5, int y5, int x6, int y6) {
+        double result2 = Math.sqrt(Math.pow((x6 - x5), 2) + Math.pow((y6 - y5), 2));
+        return result2;
+    }
+
     public static void main(String[] args) {
-        double result = Point.distance(30, 60, 55, 20);
-        System.out.println("result (30, 60) to (50, 20) " + result);
+        double result = Point.distance(0, 0, 2, 0);
+        System.out.println("result (0, 0) to (2, 0) " + result);
+        double result1 = Point.distance1(11, 22, 33, 44);
+        System.out.println("result (11, 22) to (33, 44) " + result1);
+        double result2 = Point.distance2(0, 1, 8, 0);
+        System.out.println("result (0, 1) to (8, 0) " + result2);
     }
 }
