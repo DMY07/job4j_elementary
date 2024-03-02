422ffa3e186a0ff1fdc439c7ed61062f6448d813
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-03-02 23:15:25 +0500

    Операции сравнения [#235631]

--- src/main/java/ru/job4j/condition/Liken.java
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -2,17 +2,20 @@ package ru.job4j.condition;
 
 public class Liken {
 
-
     public static void main(String[] args) {
         int first = 10;
         int second = 9;
         boolean result = first > second;
                 System.out.println(result);
 
-        boolean result1 = first < second;
-                System.out.println(result1);
+        int first1 = 7;
+        int second2 = 10;
+        boolean result1 = first1 < second2;
+        System.out.println(result1);
 
-        boolean result2 = first == second;
+        int f3 = 5;
+        int s3 = 5;
+        boolean result2 = f3 == s3;
                 System.out.println(result2);
     }
 }
