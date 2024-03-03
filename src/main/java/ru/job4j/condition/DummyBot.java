ac6d319b214894b001d1fda51fb56e8a4ff11218
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-03-03 19:10:56 +0500

    4.1.2. Глупый бот

--- src/main/java/ru/job4j/condition/DummyBot.java
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -4,9 +4,9 @@ public class DummyBot {
     public static String answer(String question) {
         String result = "I don't know. Please, ask another question.";
         if ("Hi, Bot.".equals(question)) {
-        result = "Hi, SmartAss.";
+            result = "Hi, SmartAss.";
         } else if ("Bye.".equals(question)) {
-        result = "See you later.";
+            result = "See you later.";
         }
         return result;
     }
@@ -16,5 +16,5 @@ public class DummyBot {
         System.out.println(result);
         result = DummyBot.answer("Bye.");
         System.out.println(result);
-        }
+    }
 }
