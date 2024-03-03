22c817cb6d96503bad12aca11108bac9a2ded432
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-03-03 20:50:18 +0500

    4.2. Тернарное сравнение

--- src/main/java/ru/job4j/condition/MAX.java
+++ src/main/java/ru/job4j/condition/MAX.java
@@ -2,10 +2,8 @@ package ru.job4j.condition;
 
 public class MAX {
     public static int max(int left, int right) {
-        boolean number = left > right;
-        right = 2;
-        left = 1;
-        int result = number ? left : right;
+        boolean condition = true;
+        int result = condition ? left : right;
         return result;
     }
 }
