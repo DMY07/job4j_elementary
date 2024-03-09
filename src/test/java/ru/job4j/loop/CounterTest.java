f31007d3a818bd17b7428f0124d4b466b2ea6e8c
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-03-09 22:34:59 +0500

    5.0.1. Сумма чисел.

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -12,31 +12,35 @@ class CounterTest {
     void s0f10r55() {
     int start = 0;
     int finish = 10;
+    int result = Counter.sum(start, finish);
     int expected = 55;
-    assertThat(sum(start, finish)).isEqualTo(expected);
+    assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void s3f8r33() {
         int start = 3;
         int finish = 8;
+        int result = Counter.sum(start, finish);
         int expected = 33;
-        assertThat(sum(start, finish)).isEqualTo(expected);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void s1f1r1() {
         int start = 1;
         int finish = 1;
+        int result = Counter.sum(start, finish);
         int expected = 1;
-        assertThat(sum(start, finish)).isEqualTo(expected);
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void s10f0r0() {
         int start = 10;
         int finish = 0;
+        int result = Counter.sum(start, finish);
         int expected = 0;
-        assertThat(sum(start, finish)).isEqualTo(expected);
+        assertThat(result).isEqualTo(expected);
     }
 }
