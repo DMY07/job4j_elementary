4742dbd54364fdb5e5bfc7d70206cc6d3019e9dc
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-03-09 23:00:13 +0500

    5.1. Подсчет суммы чётных чисел в диапазоне

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -10,11 +10,11 @@ class CounterTest {
 
     @Test
     void s0f10r55() {
-    int start = 0;
-    int finish = 10;
-    int result = Counter.sum(start, finish);
-    int expected = 55;
-    assertThat(result).isEqualTo(expected);
+        int start = 0;
+        int finish = 10;
+        int result = Counter.sum(start, finish);
+        int expected = 55;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
@@ -41,6 +41,42 @@ class CounterTest {
         int finish = 0;
         int result = Counter.sum(start, finish);
         int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFrom3To8Then18() {
+        int start = 3;
+        int finish = 8;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 18;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFrom5To7Then6() {
+        int start = 5;
+        int finish = 7;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 6;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFromMin7To12Then30() {
+        int start = -7;
+        int finish = 12;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
         assertThat(result).isEqualTo(expected);
     }
 }
