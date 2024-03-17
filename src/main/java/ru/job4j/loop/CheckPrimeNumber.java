b6ef3689f1f327be7b48c5bc383ba66069abd5f5
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-03-18 00:03:57 +0500

    5.5. Простое число (оператор прерывания break)

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,16 +2,11 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        int hold = 0;
+        boolean prime = number > 1;
         for (int index = 2; index <= number - 1; index++) {
-            hold += index;
-            if (hold == number) {
+            if (number % index == 0) {
+                prime = false;
                 break;
-            } else if (number % hold == 0) {
-                break;
-            } else  {
-                return false;
             }
             }
         return prime;
