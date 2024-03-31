0d80751abe104e0dc479f954fb18e5132322ab30
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-03-31 21:53:44 +0500

    5.9. Крест в псевдографике.

--- src/main/java/ru/job4j/loop/Slash.java
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -21,7 +21,7 @@ public class Slash {
     }
 
     public static void main(String[] args) {
-        System.out.println("Draw by 33");
+        System.out.println("Draw by 123");
         draw(3);
         System.out.println("Draw by 5");
         draw(5);
