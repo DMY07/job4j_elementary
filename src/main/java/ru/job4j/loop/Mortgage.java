6b7b20a98708ba1b54a6323de8f3579b2178c6cd
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-03-31 21:33:35 +0500

    5.7. Ипотека [#156315 #487891]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -6,8 +6,7 @@ public class Mortgage {
         while (amount > 0) {
             amount = (amount * (percent / 100)) - salary;
             year++;
-            salary++;
-        }
+            }
         return year;
     }
 }
