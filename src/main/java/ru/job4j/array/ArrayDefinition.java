6da217f1440293ed58b916dfe01a874d54ae3e57
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-04-01 01:49:51 +0500

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] name = new String[4];
+        name[0] = "Anna";
+        name[1] = "Коля";
+        name[2] = "Маша";
+        name[3] = "Даша";
+        System.out.println(name[3]);
+        System.out.println(name[2]);
+        System.out.println(name[1]);
+        System.out.println(name[0]);
     }
 }
