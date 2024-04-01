4d3d10e00ed6f24d05a5d8526aa51d12c3c322e4
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-04-01 22:24:37 +0500

    5.7. Ипотека [#156315 #487891]

--- src/test/java/ru/job4j/loop/MortgageTest.java
+++ src/test/java/ru/job4j/loop/MortgageTest.java
@@ -18,6 +18,16 @@ class MortgageTest {
     }
 
     @Test
+    void whenAmount100Salary120Percent50ThenYear2() {
+        int amount = 100;
+        int salary = 120;
+        double percent = 50;
+        int result = Mortgage.year(amount, salary, percent);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
     void whenAmount1000Salary1200Percent20ThenYear1() {
         int amount = 1000;
         int salary = 1200;
