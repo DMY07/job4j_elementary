b6992ff738897d04b6b551403bcde7a73b916de4
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-04-07 02:11:08 +0500

    6.4.1. Сравнить последние элементы двух массивов

--- src/main/java/ru/job4j/array/EqualLast.java
+++ src/main/java/ru/job4j/array/EqualLast.java
@@ -2,11 +2,6 @@ package ru.job4j.array;
 
 public class EqualLast {
     public static boolean check(int[] left, int[] right) {
-        boolean result = true;
-        if (left.length - 1 == right.length - 1) {
-            return true;
-        } else {
-            return false;
-        }
+        return left[left.length - 1] == right[right.length - 1];
     }
 }
