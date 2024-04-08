8a6af031768063bf2bee467f470bc492c74af415
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-04-09 00:25:28 +0500

    6.4.3. Слово заканчивается на ... [#156317 #490332]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -2,10 +2,10 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] postfix) {
-        boolean result = false;
+        boolean result = true;
         for (int i = 0; i < postfix.length; i++) {
-            if (postfix[i] != word[i]) {
-                result = true;
+            if (postfix[i] <= word[i]) {
+                result = false;
                 break;
             }
             }
