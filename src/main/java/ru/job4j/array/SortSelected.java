43d910b21265e402c599e8c49777bd4acc08b592
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-04-15 00:28:47 +0500

    6.5.3. Сортировка выборкой.

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -2,19 +2,13 @@ package ru.job4j.array;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
-        for (int index = 0; index <= data.length - 1; index++) {
+        for (int index = 0; index < data.length - 1; index++) {
             int min = MinDiapason.findMin(data, index, data.length - 1);
-            if (data[index] <= min) {
-                min = data[index];
-                int i = FindLoop.indexInRange(data, min, index, data.length - 1);
-                if (data[i] == index) {
-                    break;
-                }
-                int[] temp = SwitchArray.swap(data, index, data[0]);
-                data[i] = data[data.length - 1];
-                data[data.length - 1] = data[i];
-            }
-            }
+            int i = FindLoop.indexInRange(data, min, index, data.length - 1);
+                SwitchArray.swap(data, i, index);
+                    }
+
         return data;
     }
-}
+
+    }
