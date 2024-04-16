1e31fca99d72a1a8451c529d6b352cd38a98ee3a
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-04-17 00:54:23 +0500

    6.7.1. Моно строка в матрице.

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -5,13 +5,15 @@ public class MatrixCheck {
         boolean result = true;
         for (row = 0; row < board.length; row++) {
             for (int cell = 0; cell < board[row].length; cell++) {
-                if (board[row][cell] == 0) {
-                    board[row][cell] = 1;
-                    result = false;
-                    break;
+                for (int index = 0; index <= board[row][cell]; index++) {
+                    if (board[row][cell] == index) {
+                        result = false;
+                        break;
+                    }
                 }
             }
-            }
+
+        }
         return result;
     }
 }
