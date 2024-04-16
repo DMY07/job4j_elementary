8553c58e8d8205a121d565e4bedecf3ebef25e58
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-04-17 02:10:24 +0500

    6.8. Метод двух указателей

---
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class TwoNumberSum {
+    public static int[] getIndexes(int[] array, int target) {
+        int i = 0;
+        int j = 0;
+        while (j < array.length) {
+            if (array[i] + array[j] == target) {
+                break;
+            }
+            if (array[i] + array[j] != target) {
+                i++;
+                j++;
+            }
+            }
+        return new int[0];
+    }
+}
