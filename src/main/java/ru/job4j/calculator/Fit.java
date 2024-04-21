0ffdd550e1c755868ecf2c6419829b848e02fa6e
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-04-22 00:05:25 +0500

    Codium. Нейросети спешат на помощь.

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,7 +2,7 @@ package ru.job4j.calculator;
 
 public class Fit {
 
-    public static double calculatemanWeight(short heightMan) {
+    public static double calculateManWeight(short heightMan) {
         double result = (heightMan - 100) * 1.15;
         return result;
     }
@@ -15,7 +15,7 @@ public class Fit {
     public static void main(String[] args) {
         short heightMan = 187;
         short heightWoman = 170;
-        double man = Fit.calculatemanWeight(heightMan);
+        double man = Fit.calculateManWeight(heightMan);
         System.out.println("Man 187 is " + man);
         double woman = Fit.calculateWomanWeight(heightWoman);
         System.out.println("Woman 170 is " + woman);
