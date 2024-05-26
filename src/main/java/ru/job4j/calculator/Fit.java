0a21def6f1c5c4da537f9bd46a3a4ba9b2d86ca7
git@github.com:DMY07/-job4j_elementary.git <dpl_inf0@mail.ru>
2024-05-26 20:12:43 +0500

    GPT. Нейросети спешат на помощь. [#505156]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -1,22 +1,24 @@
 package ru.job4j.calculator;
 
 public class Fit {
-    public static double calculateManWeight(short heightMan) {
-        double result = (heightMan - 100) * 1.15;
-        return result;
+    private static final double COEFFICIENT = 1.15;
+    private static final int MAN_OFFSET = 100;
+    private static final int WOMAN_OFFSET = 110;
+
+    public static double calculateManIdealWeight(short height) {
+        return (height - MAN_OFFSET) * COEFFICIENT;
     }
 
-    public static double calculateWomanWeight(short heightWoman) {
-        double result = (heightWoman - 110) * 1.15;
-        return result;
+    public static double calculateWomanIdealWeight(short height) {
+        return (height - WOMAN_OFFSET) * COEFFICIENT;
     }
 
     public static void main(String[] args) {
         short heightMan = 187;
         short heightWoman = 170;
-        double man = Fit.calculateManWeight(heightMan);
+        double man = Fit.calculateManIdealWeight(heightMan);
         System.out.println("Man 187 is " + man);
-        double woman = Fit.calculateWomanWeight(heightWoman);
+        double woman = Fit.calculateWomanIdealWeight(heightWoman);
         System.out.println("Woman 170 is " + woman);
     }
 }
--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -12,7 +12,7 @@ class FitTest {
     void whenMan180Then92() {
         short input = 180;
         double expected = 92;
-        double output = Fit.calculateManWeight(input);
+        double output = Fit.calculateManIdealWeight(input);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
@@ -20,7 +20,7 @@ class FitTest {
     void whenWoman170Then69() {
         short input = 170;
         double expected = 69;
-        double output = Fit.calculateWomanWeight(input);
+        double output = Fit.calculateWomanIdealWeight(input);
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
